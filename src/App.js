d3618234d14feec99c8b8cea486dcc734218e03b
nitish <nks199kumar@gmail.com>
2024-10-16 23:32:17 +0530

    5th

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import { Fragment } from 'react';
+
 import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals.js';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+
 function App() {
   return (
-    <>
-    <Header/>
-    <main>
+    <Fragment>
+      <Cart />
+      <Header />
+      <main>
         <Meals />
-    </main>
-
-    </>
-
-  )
+      </main>
+    </Fragment>
+  );
 }
 
 export default App;
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,5 +1,5 @@
 import Input from '../../UI/Input';
-import classes from './MealItem.module.css'
+import classes from './MealItemForm.module.css'
 
 const MealItemForm = (props) => {
   return (
